794d2932e70cf5ea6a6d9988f9f1db87c49b82a5
henan.wang <henan.wang@henan.wang's MacBook Pro>
2018-07-03 18:23:37 +0800

    predict funny_test

--- src/predict_funny.sh
+++ src/predict_funny.sh
@@ -1,2 +1,8 @@
-python3 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/funny --pretrained_model_folder=../trained_models/conll_2003_en
+#mkdir -p ../data/funny/deploy
+#cp ../../../../dataset/tag/tags_content.raw.20180703 .
+#cd ../data/funny/deploy
+#split -l 1 -d --suffix-length=6 --additional-suffix=.txt tags_content.raw.20180703
+#cd ../../../src/
+
+python3 main.py --train_model=False --use_pretrained_model=True --dataset_text_folder=../data/funny_test --pretrained_model_folder=../trained_models/conll_2003_en
 
