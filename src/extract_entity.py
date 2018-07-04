aee6f70905c3d4453df7d15c5747946ecf061710
Ubuntu <ubuntu@ip-192-168-6-51.ec2.internal>
2018-07-04 08:48:17 +0000

    improve extract_entity.py

--- src/extract_entity.py
+++ src/extract_entity.py
@@ -13,25 +13,36 @@ def extract_entity(filename=FILENAME):
         for line in lines:
             all_cnt += 1
             params = line.strip().split(SEP)
-            if len(params) == 7:
-                entity_cnt += 1
+            if len(params) == 6:
                 keyword = params[0]
                 docid = params[1]
-                entity = params[6]
-                if keyword not in kes:
-                    kes[keyword] = entity
+                entity = params[5]
+                if entity == 'O':
+                    not_entity_cnt += 1
                 else:
-                    if kes[keyword] != entity:
-                        print(keyword, kes[keyword], entity)
-            if len(params) == 6:
-                not_entity_cnt += 1
+                    entity_cnt += 1
+                    if keyword not in kes:
+                        kes[keyword] = {}
+                    if entity not in kes[keyword]:
+                        kes[keyword][entity] = 0
+                    kes[keyword][entity] += 1
             else:
                 pass
+    print('all_cnt:', all_cnt)
+    print('entity_cnt:', entity_cnt)
+    print('not_entity_cnt:', not_entity_cnt)
     print('kes:', len(kes))
+    frequent_entity_cnt = 0
     with open(filename+'.kes', 'w') as fout:
         for k,v in kes.items():
-            fout.write(k+SEP+v+'\n')
-
+            v_sum = 0
+            for k1,v1 in v.items():
+                #fout.write(k+SEP+k1+SEP+str(v1)+'\n')
+                v_sum += v1
+            if v_sum >= 100:
+                fout.write(k+SEP+str(v_sum)+'\n')
+                frequent_entity_cnt += 1
+    print('frequent_entity_cnt:', frequent_entity_cnt)
 
 if __name__ == '__main__':
     extract_entity()
